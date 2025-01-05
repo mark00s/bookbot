d15c99184e3863f6ee91374933737ba693e98793
Marek Jakóbiak <marek.jakobiak@gmail.com>
2025-01-05 19:24:18 +0100

    feat: print_report function

--- main.py
+++ main.py
@@ -8,11 +8,15 @@ def main():
     file_paths = []
     try:
         file_paths = save_books()
-        print_books(file_paths)
+        for file_path in file_paths:
+            book_text = get_book(file_path["path"])
+            word_count = count_words(book_text)
+            book_chars = count_chars(book_text)
+
+            print_report(file_path["path"], word_count, book_chars)
+
     except Exception as e:
         print(e)
-
-
 
     return 0
 
@@ -57,5 +61,17 @@ def count_chars(book_text):
             result[char] = count + 1
     
     return dict(sorted(result.items(), key=lambda item: item[1], reverse=True))
+
+def print_report(path, wc, book_chars):
+    print(f"--- Begin report of {path} ---")
+    print(f"{wc} words found in the document")
+    print("")
+    for book_char in book_chars:
+        count = book_chars[book_char]
+        lane = f"The '{book_char}' character was found {count} time"
+        if count > 1:
+            lane += "s"
+        print(lane)
+    print("--- End report ---")
 
 main()
